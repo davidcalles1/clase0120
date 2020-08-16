d6e043223caadd9cd2d0874142095b2f48bef805
nelson calles <cc0324032019@unab.edu.sv>
2020-08-16 08:19:47 -0500

    Creacion

---
+++ clase0120/src/main/java/com/unab/edu/sv/clase0120/clase0120.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.edu.sv.clase0120;
+
+/**
+ *
+ * @author nelson
+ */
+public class clase0120 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String hola = "hello word";
+        var numero = 2;
+        var numero2 = 2;
+        System.out.println(numero+numero2);
+    }
+    
+}
